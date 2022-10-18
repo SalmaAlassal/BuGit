08421d9275d77353d11c1d020d59bffbd5b18678
Abdo sayed <abdulrhmansayed70@gmail.com>
2022-10-18 14:17:06 +0200

    valid sequance bug

---
+++ valid sequance.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long 
+
+int main() {
+	string s; cin >> s; 
+	stack<char>st;
+	for (auto it : s) {
+		if (it == '(') 
+		{ 
+			st.push(it);
+		}
+		else
+		{
+			st.pop();
+		}
+	}
+	cout << (st.empty() ? "YES" : "NO");
+}
