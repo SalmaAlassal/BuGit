b9d4d2e4bcb2c82d61a07ac1b7f9213e414e2a11
Badr <badrmohsamedashraf@gmail.com>
2022-10-20 14:45:59 +0200

    empty space

--- 9-Wait a mintue who are you.cpp
+++ 9-Wait a mintue who are you.cpp
@@ -8,6 +8,6 @@ int main()
         cout << "wait a minute who are you?\n";
         cin >> name;
     }
-    cout << "oh, hi " << name << endl;
+    cout << "oh,       hi " << name << endl;
     return 0;
 }
